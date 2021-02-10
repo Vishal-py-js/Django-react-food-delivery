509a4dbcb668c1d1c485def6445099d252542747
Vishal Kumar <vskkr007@gmail.com>
2021-02-10 15:43:56 +0530

    added AWS S3 credentials

--- delivery/settings.py
+++ delivery/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_multiple_model',
     'corsheaders',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -148,13 +149,19 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+AWS_ACCESS_KEY_ID = os.environ.get('KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('BUCKET_NAME')
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
