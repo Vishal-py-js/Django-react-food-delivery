ef27200acd8ffc4025e791c1bf2a75ceab706800
Vishal Kumar <vskkr007@gmail.com>
2021-02-11 21:31:28 +0530

    configured whitenoise

--- delivery/settings.py
+++ delivery/settings.py
@@ -147,10 +147,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 
 django_heroku.settings(locals())
