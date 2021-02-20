23b74d98c37b174a5fddd576c5b9f2d1b1c57bb2
Vishal Kumar <vskkr007@gmail.com>
2021-02-20 18:30:36 +0530

    added zipcode to the ShippingAdress model

--- food/models.py
+++ food/models.py
@@ -78,3 +78,7 @@ class ShippingAddress(models.Model):
     address = models.CharField(max_length=50)
     city = models.CharField(max_length=30)
     state = models.CharField(max_length=20)
+    zipcode = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f"{self.user}'s address'"
